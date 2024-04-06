240ef779ae7a7c37fc3b56c8170644de0612273c
Fercho1118 <rue23748@uvg.edu.gt>
2024-04-06 12:45:13 -0600

    Clase Paciente
    Se programó la clase que permite la comparación entre instancias de Paciente basadas en el código de emergencia.

---
+++ hdt8/src/main/java/uvg/edu/gt/Paciente.java
@@ -0,0 +1,27 @@
+package uvg.edu.gt;
+
+public class Paciente implements Comparable<Paciente>{
+    private String nombre;
+    private String sintoma;
+    private char codigoEmergencia;
+    
+    public Paciente(String nombre, String sintoma, char codigoEmergencia) {
+        this.nombre = nombre;
+        this.sintoma = sintoma;
+        this.codigoEmergencia = codigoEmergencia;
+    }
+
+    public String getNombre() { return nombre; }
+    public String getSintoma() { return sintoma; }
+    public char getCodigoEmergencia() { return codigoEmergencia; }
+
+    @Override
+    public int compareTo(Paciente o) {
+        return this.codigoEmergencia - o.codigoEmergencia;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + ", " + sintoma + ", " + codigoEmergencia;
+    }
+}
